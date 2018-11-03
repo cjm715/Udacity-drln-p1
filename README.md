13ba95e9f050ee06e16dc1b38dd02ef845d270ab
cjm715 <chris.john.miles@gmail.com>
2018-11-02 22:25:09 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Udacity Project
+# Udacity Project 1 
 
 ## Introduction
 
-This project explores Q-learning on the Unity Banana environment.
+This project explores Q-learning on the Unity Banana environment. For full details,
+see `report/project.pdf`.
 
 ## Usage
+
+Clone repo and run `python src/dqn_train.py` from root. 
 
--- src/dqn_train.py
+++ src/dqn_train.py
@@ -82,7 +82,7 @@ def dqn(n_episodes=2000, max_t=1000, eps_start=0.9, eps_end=0.01, eps_decay=0.95
         if i_episode % 100 == 0:
             print('\rEpisode {}\tAverage Score: {:.2f} \t Epsilon: {}'.format(
                 i_episode, np.mean(scores_window), eps))
-        if np.mean(scores_window) >= 1.0:
+        if np.mean(scores_window) >= 13.0:
             print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(
                 i_episode, np.mean(scores_window)))
             torch.save(agent.qnetwork_local.state_dict(),
